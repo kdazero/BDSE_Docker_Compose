6571b0b649424d3392f292f7e8b8e79991f889a0
可憐的帕魯 <kdazero.mail@gmail.com>
2024-12-22 20:28:04 +0800

    作業更新

--- hadoophw/backup.sh
+++ hadoophw/backup.sh
@@ -5,7 +5,7 @@ sudo cat /etc/shadow > backup_shadow
 cat /etc/group > backup_group
 
 echo "建立壓縮檔"
-tar zcvf backup.tar.gz backup_passwd backup_shadow backup_group /home
+tar -zcpvf backup.tar.gz backup_passwd backup_shadow backup_group /home
 
 echo "上傳中..."
 hdfs dfs -put backup.tar.gz /user/bigred/
--- hadoophw/restore.sh
+++ hadoophw/restore.sh
@@ -8,14 +8,14 @@ echo "下載備份檔..."
 hdfs dfs -get /user/bigred/ zcxf backup.tar.gz
 
 echo "解壓中..."
-tar zcxf backup.tar.gz
+tar -zxpvf backup.tar.gz
 
 ecoh "還原中..."
 sudo backup_passwd > /etc/passwd
 sudo backup_shadow > /etc/shadow
 sudo backup_group > /etc/group
 
-home > /home
+sudo home > /home
 
 echo "清理暫存檔..."
 rm -rf tmpfile
