0d1b03422e4a3777b6ded52a1e1b864f52cce4e4
可憐的帕魯 <kdazero.mail@gmail.com>
2024-12-24 01:32:24 +0800

    回檔

--- py_m/app.py
+++ py_m/app.py
@@ -30,6 +30,17 @@ def index():
 
         return render_template('index.html', data=data)
 
+        # 渲染 HTML 模板並傳遞資料
+        #rendered_html = render_template('index.html', data=data)
+
+        # 將渲染好的 HTML 儲存到 shared_data 資料夾
+        #with open('/data/index.html', 'w') as f:
+        #    f.write(rendered_html)
+
+        #return redirect(url_for('static', filename='index.html'))  # 重新導向到 Nginx 提供的靜態檔案
+        #return rendered_html
     except Exception as e:
-        # docker container logs {container_name} 看錯誤訊息
         return str(e)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
